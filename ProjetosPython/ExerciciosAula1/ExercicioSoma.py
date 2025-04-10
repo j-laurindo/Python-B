216a542c2bb0a2581730b6820c40bc97ed99f8b8
Julia Laurindo <160611630+j-laurindo@users.noreply.github.com>
2025-04-10 09:15:57 -0300

    commit 10-04

---
+++ ProjetosPython/ExerciciosAula1/ExercicioSoma.py
@@ -0,0 +1,11 @@
+# Programa da Soma #
+
+# Solicita ao usuário o valor de X e Y
+X = int(input("Insira um valor para X: "))
+Y = int(input("Insira um valor para Y: "))
+
+# Realiza o calculo da soma de X e Y
+soma = Y + X
+
+# Exibe o resultado da soma para o usuário
+print(f"\nResultado: {X} + {Y} = {soma}")
